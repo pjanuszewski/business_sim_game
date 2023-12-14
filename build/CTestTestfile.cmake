e1a6180450011d3ee94675ac0f7b62f409918fa3
Pawel Januszewski <akurat444@gmail.com>
2023-12-14 17:37:01 +0100

    Some more trivial changes

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pawelj/Git_repos/business_sim_game
+# Build directory: /home/pawelj/Git_repos/business_sim_game/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
