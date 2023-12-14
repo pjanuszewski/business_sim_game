794fed8033ed7918fd727f39851d15c8e1edb189
Pawel Januszewski <akurat444@gmail.com>
2023-12-14 17:39:15 +0100

    gitignore fix

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pawelj/Git_repos/business_sim_game
-# Build directory: /home/pawelj/Git_repos/business_sim_game/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
